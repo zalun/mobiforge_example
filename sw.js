84402d55a4e7faf086ef03319c3dc2dc9219f934
Piotr Zalewa <piotr@zalewa.info>
2015-10-20 12:49:29 +0200

    movide to mobiforge directory

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ self.addEventListener('push', function(event) {
 
   //We wait for data fetch and notification promises
   event.waitUntil(
-    fetch("https://trivial.zalewa.info/push/latest.json", fetchInit).then(function(res) {
+    fetch("https://trivial.zalewa.info/mobiforge/push/latest.json", fetchInit).then(function(res) {
       return res.json().then(function(notificationData) {
         // Show notification
         console.log(notificationData);
